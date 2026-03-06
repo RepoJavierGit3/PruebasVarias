7ea7817f329d35a515c9d368b7e20f60fa0f0a64
RepoJavierGit3 <gjcumbal@gmail.com>
2026-03-05 22:31:47 -0500

    Delete frontend directory
    Delete frontend directory

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://tu-dominio.com/api',
-  apiTimeout: 30000,
-  enableDebug: false
-};
--- frontend/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://192.168.100.27:8000/api',
-  apiTimeout: 30000,
-  enableDebug: true
-};
